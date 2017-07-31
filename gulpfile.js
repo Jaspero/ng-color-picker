e31ccf66fd2134491e50700775a341ae856856eb
Filip Lauc <filip.lauc93@gmail.com>
2017-07-31 22:15:56 +0200

    chore(package): minor updates :hammer:

--- gulpfile.js
+++ gulpfile.js
@@ -131,7 +131,7 @@ gulp.task('rollup:umd', function () {
       // The name to use for the module for UMD/IIFE bundles
       // (required for bundles with exports)
       // See https://github.com/rollup/rollup/wiki/JavaScript-API#modulename
-      moduleName: 'test-lib',
+      moduleName: 'ng-color-picker',
 
       // See https://github.com/rollup/rollup/wiki/JavaScript-API#globals
       globals: {
@@ -139,7 +139,7 @@ gulp.task('rollup:umd', function () {
       }
 
     }))
-    .pipe(rename('test-lib.umd.js'))
+    .pipe(rename('ng-color-picker.umd.js'))
     .pipe(gulp.dest(distFolder));
 });
 
